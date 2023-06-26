ae9b1db6083aab6406a1bf7f580e3483f9c608be
mateowebdev <mateocernaz01@gmail.com>
2023-06-26 20:39:20 -0300

    mensaje details

--- src/customHooks/useMsgWapp.jsx
+++ src/customHooks/useMsgWapp.jsx
@@ -17,6 +17,8 @@ export default function useMensageWap(
   const NOMBRE_EVENTO = `
   *${nombre}*
   📍 ${lugar}
+  ${maps ? maps : ""}
+  📅 ${dia}${hora ? ' | '+hora+" hs" : ""}
   `;
   const LUGAR_EVENTO = `%0D%0A📍 ${lugar}%0D%0A`;
   const LINK_EVENTO = `%0D%0A${maps}%0D%0A`;
