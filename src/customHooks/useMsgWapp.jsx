90932038a24c134483cf452872e0c0879745e787
mateowebdev <mateocernaz01@gmail.com>
2023-06-27 02:48:44 -0300

    mensaje details

--- src/customHooks/useMsgWapp.jsx
+++ src/customHooks/useMsgWapp.jsx
@@ -12,15 +12,19 @@ export default function useMensageWap(
 ) {
   const lista = Array.from({ length: listado }, (v, i) => i + 1);
 
-  const NOMBRE_EVENTO = `*${nombre.toUpperCase()}*`;
-  const LUGAR_EVENTO = `${lugar.length > 3 ? "📍 "+lugar : "📍 Lugar a definir."}`;
-  const LINK_EVENTO = `${maps ? maps : ""}`;
+  const NOMBRE_EVENTO = `*${nombre.toUpperCase().trim()}*
+  `;
+  const LUGAR_EVENTO = `${lugar.length > 3 ? "📍 "+lugar.trim() : "📍 Lugar a definir."}
+  `;
+  const LINK_EVENTO = `${maps ? maps.trim() : ""}
+  `;
 
-  const DIA_EVENTO = `${dia ? "📅 "+dia : "📅 Fecha a definir."}`;
-  const HORARIO_EVENTO = `${hora ? "⏰ "+hora+ " hs" : ""}`;
-  const FECHA_EVENTO = `${DIA_EVENTO} ${HORARIO_EVENTO}`;
+  const DIA_EVENTO = `${dia ? "📅 "+dia.trim() : "📅 Fecha a definir."}`;
+  const HORARIO_EVENTO = `${hora ? "⏰ "+hora.trim()+ " hs" : ""}`;
+  const FECHA_EVENTO = `${DIA_EVENTO} ${HORARIO_EVENTO}
+  `;
 
-  const DESCRIPCION_EVENTO = `${descripcion ? "📝 "+descripcion : ""}`;
+  const DESCRIPCION_EVENTO = `${descripcion ? "📝 _"+descripcion.trim()+"_" : ""}`;
 
   const LISTADO_EVENTO = `${lista
     .map((item, index) => {
@@ -39,10 +43,10 @@ export default function useMensageWap(
 
   const FOOTER = `
   
-  
+
   _Creado con: https://wapp.com_`;
 
-  const mensaje = `${NOMBRE_EVENTO}${LUGAR_EVENTO}${LINK_EVENTO}${FECHA_EVENTO}${DESCRIPCION_EVENTO}${LISTADO_EVENTO}${BAJAS_EVENTO}${FOOTER}`;
+  const mensaje = NOMBRE_EVENTO+LUGAR_EVENTO+LINK_EVENTO+FECHA_EVENTO+DESCRIPCION_EVENTO+LISTADO_EVENTO+BAJAS_EVENTO+FOOTER;
 
   const crearMsgWap = () => {
     return mensaje;
